99e501838605750910d39bd02e60d6c06cc7f0a1
Satyam Pandey <satyampandey88@gmail.com>
2024-10-05 14:55:17 +0530

    coderefactor

---
+++ src/main/java/com/example/fusion_ms/devops.java
@@ -0,0 +1,11 @@
+package com.example.fusion_ms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class devops {
+    @GetMapping("/devops")
+    public String getData() {return  "Welcome to Devops Class" ; }
+}
+
