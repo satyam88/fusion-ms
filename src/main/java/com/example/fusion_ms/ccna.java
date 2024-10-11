c2bee2ac5e3f2901d2bbf92c3f01f58ae09d605f
Satyam Pandey <satyampandey88@gmail.com>
2024-10-11 08:48:01 +0530

    adding java class

---
+++ src/main/java/com/example/fusion_ms/ccna.java
@@ -0,0 +1,10 @@
+package com.example.fusion_ms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ccna {
+    @GetMapping("/ccna")
+    public String getData() {return  "Welcome to ccna Class" ; }
+}
